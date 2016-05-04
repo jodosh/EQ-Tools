1d373c1fc8fdd216992ab3e03e78222949a59e4a
jodosh <github@antred.com>
2016-05-04 09:28:37 -0500

    fixed tinyURL timeout issue

--- WardHarvest/generate.py
+++ WardHarvest/generate.py
@@ -2,7 +2,7 @@ import csv
 from pyshorteners import Shortener
 import urllib
 
-startHTML00 = "<html>\n<head>\n<title>Ward Harvest</title>\n<style type=text/css>\ntable.gridtable {\nfont-family: verdana,arial,sans-serif;\nfont-size:11px;\ncolor:#333333;\nborder-width: 1px;\n"
+startHTML00 = "<html>\n<head>\n<meta charset=\"UTF-8\">\n<title>Ward Harvest</title>\n<style type=text/css>\ntable.gridtable {\nfont-family: verdana,arial,sans-serif;\nfont-size:11px;\ncolor:#333333;\nborder-width: 1px;\n"
 startHTML01 = "border-color: #666666;\nborder-collapse: collapse;\nwidth:95%;\nmargin-top:10px;\n}\ntable.gridtable th {\nborder-width: 1px;\npadding: 8px;\nborder-style: solid;\nborder-color: #666666;\n"
 startHTML02 = "background-color: #dedede;\n}\ntable.gridtable td {\nborder-width: 1px;\npadding: 8px;\nborder-style: solid;\nborder-color: #666666;\nbackground-color: #ffffff;\nwidth: 25%;\n}\n"
 startHTML03 = "table.gridtable tr:nth-child(even) {\nfont-weight: bold;\nfont-size:16px;\n}\n</style>\n<style type=\"text/css\">\ntable { page-break-inside:avoid }\ntr    { page-break-inside:avoid; page-break-after:avoid }\n"
@@ -29,7 +29,7 @@ with open('Sheet1.tsv', 'rb') as csvFile:
 
 		url = row[5]
 		print(url)
-		shortener = Shortener('TinyurlShortener')
+		shortener = Shortener('TinyurlShortener', timeout=9000)
 		shortURL = shortener.short(url)
 
 		URL = shortener.qrcode().replace("120x120","220x220")
@@ -53,9 +53,9 @@ with open('Sheet1.tsv', 'rb') as csvFile:
 		tableMiddle += "</td><td><img src=\""
 		tableMiddle += FILENAME # Should Be QR Filename
 		tableMiddle += "\" /></td>\n"
-		tableEnd = "<td colspan=2 valign=top><input type=\"checkbox\"> Made Contact<br>\n<input type=\"checkbox\"> Intrested in Contact<br>\n<input type=\"checkbox\"> Moved?<br>\n"
+		tableEnd = "<td colspan=2 valign=top><input type=\"checkbox\"> Made Contact<br>\n<input type=\"checkbox\"> Interested in Contact<br>\n<input type=\"checkbox\"> Moved?<br>\n"
 		tableEnd += row[4] #Notes
-		tableEnd += "</td></table>\n"
+		tableEnd += "</td></tr><tr> <td colspan=\"4\" valign=\"top\">Assigned to: </td></tr></table>\n"
 		
 		f.write(tableStart)
 		f.write(tableMiddle)
